4cc4c80a9387a93ca3ee71276ce2941b959fcd38
Waasila Asif <wasif.bscs24seecs@seecs.edu.pk>
2026-02-11 00:14:02 +0500

    Added the ugly camera component

--- main/src/App.tsx
+++ main/src/App.tsx
@@ -1,33 +1,20 @@
 import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
+import useCamera from './hooks/useCamera'
 import './App.css'
+import { renderToString } from 'react-dom/server';
 
 function App() {
-  const [count, setCount] = useState(0)
+
+// const camera = useCamera();
+// const videoRef = camera.videoRef;
+const {videoRef} = useCamera();
+//renderToString({videoRef})
 
   return (
     <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
+      <div className="video">
+        <video ref={videoRef} autoPlay playsInline muted></video>
       </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
     </>
   )
 }
--- main/src/hooks/useCamera.ts
+++ main/src/hooks/useCamera.ts
@@ -1,7 +1,38 @@
 // useCamera Hook in ts
-import { useState, useEffect } from 'react';
+import { useEffect, useRef } from "react";
 
 const useCamera = () => {
-getSupportedConstraints();
-}
+  const videoRef = useRef(null);
+  const startCamera = async () => {
+    try {
+      const stream = await navigator.mediaDevices.getUserMedia({ video: true });
+      const videoTrack = stream.getVideoTracks()[0];
+      const capabilities = videoTrack.getCapabilities();
+      const maxWidth = capabilities.width?.max || 1920;
+      const maxHeight = capabilities.height?.max || 1080;
+      console.log(`Setting resolution to: ${maxWidth}x${maxHeight}`);
+       const constraints = {
+      width: { ideal: maxWidth },
+      height: { ideal: maxHeight }
+    };
+     await videoTrack.applyConstraints(constraints);
+    
+    console.log("Success! The camera is now running at its hardware limit.");
+    
+    // Check the current settings to confirm
+    console.log("Current Settings:", videoTrack.getSettings());
+
+      if (!videoRef.current)
+        console.error("Video track is not being connected to current");
+      videoRef.current.srcObject = stream;
+    } catch {
+      console.error("Error connecting to the camera");
+    }
+  };
+  useEffect(()=>{
+    startCamera();
+  },[])
+  return { videoRef };
+};
+
 export default useCamera;
