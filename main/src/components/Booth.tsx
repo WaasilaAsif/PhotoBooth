6e5ed5cd4017810a394127a36f0b5ff680c717a5
Waasila Asif <wasif.bscs24seecs@seecs.edu.pk>
2026-02-12 01:15:04 +0500

    Done with the timeout and stuff now have to make uop the loop through logic and stuff for it to work

--- main/src/components/Booth.tsx
+++ main/src/components/Booth.tsx
@@ -2,12 +2,16 @@ import * as React from 'react';
 import { useRef, useEffect } from 'react'
 import Capture from './capture';
 import Stream from './Stream';
-
+import './styles/Capture.css'
 const Booth = () =>{
  return(
   <>
-    <Stream/>
-    {<Capture/>}
+  <div className="photo-booth">
+    <div className='vid-container'>
+    {/* <Stream /> */}
+    </div>
+    <Capture/>
+    </div>
   </>
  )
 };
--- main/src/components/Capture.tsx
+++ main/src/components/Capture.tsx
@@ -3,27 +3,32 @@ import { useRef, useEffect } from 'react'
 import useCamera from '../hooks/useCamera'
 import useCanvas from '../hooks/useCanvas'
 
-type Props = {}
+type Props = {};
 
 const Capture = (props: Props) => {
   // 1. Get the video ref from your hook (The Source)
   const { videoRef } = useCamera();
   const {canvasRef, capturePhoto} = useCanvas(videoRef);
-   
+  
+  const startTimer=()=>{
+    const camera = setTimeout(capturePhoto, 5000);
+  }
 return(
   <>
-  
-  <button onClick={capturePhoto}>
-    SNAP SHOT
+  <div className="video-container">
+    <video ref = {videoRef} autoPlay playsInline muted/>
+  </div>
+  <button onClick={startTimer}>
+    Ready?
   </button>
   <div className="canvas-container">
-    <h3>Captured Photo</h3>
-    <canvas ref = {canvasRef}/>
+
+    <canvas id='photo' ref = {canvasRef}/>
   </div>
   </>
 
 )
-;
-  }
+;}
+
 
 export default Capture;
